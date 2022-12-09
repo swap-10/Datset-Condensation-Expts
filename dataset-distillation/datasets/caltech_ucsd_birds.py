cbb6572114d23dea795fe4d95c5c2ebf5c8b09cd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-09 11:18:37 +0000

    Adding tarfile member sanitization to extractall()

--- dataset-distillation/datasets/caltech_ucsd_birds.py
+++ dataset-distillation/datasets/caltech_ucsd_birds.py
@@ -305,7 +305,26 @@ class CUB200(data.Dataset):
             fullpath = os.path.abspath(os.path.join(self.root, self.filename))
             logging.info("Extract {}".format(fullpath))
             with tarfile.open(fullpath, 'r:gz') as tar:
-                tar.extractall(self.root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, self.root)
             logging.info("[DONE]")
 
     def load(self):
